11662cbe2e7349fae0875a540d54105f3c4df362
JerrymonJRY <58291290+JerrymonJRY@users.noreply.github.com>
2023-11-21 14:41:59 +0400

    s

--- frontend/src/Components/layouts/base_url.jsx
+++ frontend/src/Components/layouts/base_url.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const apiConfig = {
    // baseURL: 'http://localhost:5000',
-  baseURL: 'https://burppos.onrender.com/'
+  baseURL: 'https://burppos.onrender.com'
   };
   
   export default apiConfig;
