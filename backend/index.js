e0b7a4d82de211047735c5fd53581f5c2847634d
JerrymonJRY <58291290+JerrymonJRY@users.noreply.github.com>
2023-11-22 17:30:40 +0400

    qr

--- backend/index.js
+++ backend/index.js
@@ -1,66 +1,64 @@
-const express =require('express');
+const express = require('express');
 const bodyParser = require('body-parser');
 const dbConnect = require('./config/dbConnect');
-const app =express();
-const dotenv =require('dotenv').config();
-const cors =require('cors');
-const morgan =require('morgan');
-const cookieParser =require('cookie-parser');
-//const bodyParser = require("body-parser");
+const app = express();
+const dotenv = require('dotenv').config();
+const cors = require('cors');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
 
-//Routes
+// Routes
+const authRouter = require('./routes/authRoutes');
+const categoryRouter = require('./routes/categoryRoutes');
+const ingunitRouter = require('./routes/ingredientunitRoutes');
+const ingredientRouter = require('./routes/ingredientRoutes');
+const vatRouter = require('./routes/vatRoutes');
+const tableRouter = require('./routes/tableRoutes');
+const foodcategoryRouter = require('./routes/foodcategoryRoutes');
+const foodmenuRouter = require('./routes/foodmenuRoutes');
+const waiterRouter = require('./routes/waiterRoutes');
+const posRouter = require('./routes/posRoutes');
+const customerRouter = require('./routes/customerRoutes');
+const deliveryRouter = require('./routes/deliveryRoutes');
+const supplierRouter = require('./routes/supplierRoutes');
+const purchaseRouter = require('./routes/purchaseRoutes');
 
-const authRouter =require('./routes/authRoutes');
-const categoryRouter =require('./routes/categoryRoutes');
-const ingunitRouter =require('./routes/ingredientunitRoutes');
-const ingredientRouter =require('./routes/ingredientRoutes');
-const vatRouter =require('./routes/vatRoutes');
-const tableRouter =require('./routes/tableRoutes');
-const foodcategoryRouter =require('./routes/foodcategoryRoutes');
-const foodmenuRouter =require('./routes/foodmenuRoutes');
-const waiterRouter =require('./routes/waiterRoutes');
-const posRouter =require('./routes/posRoutes');
-const customerRouter =require('./routes/customerRoutes');
-const deliveryRouter =require('./routes/deliveryRoutes');
-const supplierRouter =require('./routes/supplierRoutes');
-const purchaseRouter =require('./routes/purchaseRoutes');
-//const posRouter =require('./routes/posRoutes');
-
-const PORT =process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
 dbConnect();
-app.use(cors());
+
+// Set up CORS middleware
 const corsOptions = {
-    origin: 'https://restaurant-app-frontend-ten.vercel.app',
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
-  
-  app.use(cors(corsOptions));
+  origin: 'https://restaurant-app-frontend-ten.vercel.app',
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan("dev"));
-app.use('/api/user',authRouter);
-app.use('/api/category',categoryRouter);
-app.use('/api/ingunit',ingunitRouter);
-app.use('/api/ingredient',ingredientRouter);
-app.use('/api/vat',vatRouter);
-app.use('/api/table',tableRouter);
-app.use('/api/foodcategory',foodcategoryRouter);
-app.use('/api/foodmenu',foodmenuRouter);
-app.use('/api/waiter',waiterRouter);
-app.use('/api/pos',posRouter);
-app.use('/api/customer',customerRouter);
-app.use('/api/delivery',deliveryRouter);
-app.use('/api/supplier',supplierRouter);
-app.use('/api/purchase',purchaseRouter);
-app.use('/',(req,res) =>{
-    res.send("Hellow From Server Side");
-});
+app.use(morgan('dev'));
 app.use(cookieParser());
 
+// Routes
+app.use('/api/user', authRouter);
+app.use('/api/category', categoryRouter);
+app.use('/api/ingunit', ingunitRouter);
+app.use('/api/ingredient', ingredientRouter);
+app.use('/api/vat', vatRouter);
+app.use('/api/table', tableRouter);
+app.use('/api/foodcategory', foodcategoryRouter);
+app.use('/api/foodmenu', foodmenuRouter);
+app.use('/api/waiter', waiterRouter);
+app.use('/api/pos', posRouter);
+app.use('/api/customer', customerRouter);
+app.use('/api/delivery', deliveryRouter);
+app.use('/api/supplier', supplierRouter);
+app.use('/api/purchase', purchaseRouter);
 
+app.use('/', (req, res) => {
+  res.send('Hello From Server Side');
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running  at PORTs ${PORT}`);
-  });
-  
+  console.log(`Server is running at PORT ${PORT}`);
+});
