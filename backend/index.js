2477718a262c750c7991cc362e4d8cc7832eb075
JerrymonJRY <58291290+JerrymonJRY@users.noreply.github.com>
2023-11-22 12:35:06 +0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,13 @@ const purchaseRouter =require('./routes/purchaseRoutes');
 
 const PORT =process.env.PORT || 4000;
 dbConnect();
-app.use(cors());
+app.use(cors(
+    {
+    orgin:["https://restaurant-app-frontend-ten.vercel.app"],
+    methods:["POST","GET"],
+    credentials:true
+    }
+));
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
