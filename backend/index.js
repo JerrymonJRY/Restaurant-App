098d8c885cb99c08dd66ce298f1123c2ef93d7c9
JerrymonJRY <58291290+JerrymonJRY@users.noreply.github.com>
2023-11-22 17:08:49 +0400

    ert

--- backend/index.js
+++ backend/index.js
@@ -47,9 +47,9 @@ app.use('/api/customer',customerRouter);
 app.use('/api/delivery',deliveryRouter);
 app.use('/api/supplier',supplierRouter);
 app.use('/api/purchase',purchaseRouter);
-app.use('/',(req,res) =>{
-    res.send("Hellow From Server Side");
-});
+// app.use('/',(req,res) =>{
+//     res.send("Hellow From Server Side");
+// });
 app.use(cookieParser());
 
 
