d88d76fc2047f77dc9f209db6c6cad7dc739bcd3
JerrymonJRY <58291290+JerrymonJRY@users.noreply.github.com>
2023-11-22 09:19:08 +0400

    zz

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -3,9 +3,7 @@ const { default: mongoose } = require("mongoose");
 const dbConnect = () => {
   try {
     const conn = mongoose.connect(process.env.MONGODB_URL);
-    mongoose.connect(conn, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(error => console.error('Error connecting to MongoDB:', error));
+    console.log("Database Connected Successfully");
   } catch (error) {
     console.log("DAtabase error");
   }
